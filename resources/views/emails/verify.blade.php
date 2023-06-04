47752b93ecde1658d13b2de9bc74dad84f6dfb4e
AmrTarekAbuElkhair <engamrtarek1992@gmail.com>
2023-06-04 15:37:51 +0200

    add verification template in blade

---
+++ resources/views/emails/verify.blade.php
@@ -0,0 +1,79 @@
+<!--<h1>Hi, {{ $name }} Welcome to Events Application</h1>-->
+<!--<p>here is your code : {{$code}}</p>-->
+<!--<p>Sending Mail from Events Application.</p>-->
+
+
+<body link="#00a5b5" vlink="#00a5b5" alink="#00a5b5">
+
+    <table class=" main contenttable" align="center" style="font-weight: normal;border-collapse: collapse;border: 0;margin-left: auto;margin-right: auto;padding: 0;font-family: Arial, sans-serif;color: #555559;background-color: white;font-size: 16px;line-height: 26px;width: 600px;">
+            <tr>
+                <td class="border" style="border-collapse: collapse;border: 1px solid #eeeff0;margin: 0;padding: 0;-webkit-text-size-adjust: none;color: #555559;font-family: Arial, sans-serif;font-size: 16px;line-height: 26px;">
+                    <table style="font-weight: normal;border-collapse: collapse;border: 0;margin: 0;padding: 0;font-family: Arial, sans-serif;">
+                     
+                        <tr>
+                            <td valign="top" class="side title" style="border-collapse: collapse;border: 0;margin: 0;padding: 20px;-webkit-text-size-adjust: none;color: #555559;font-family: Arial, sans-serif;font-size: 16px;line-height: 26px;vertical-align: top;background-color: white;border-top: none;">
+                                <table style="font-weight: normal;border-collapse: collapse;border: 0;margin: 0;padding: 0;font-family: Arial, sans-serif;">
+                                    <tr>
+                                        <td class="head-title" style="border-collapse: collapse;border: 0;margin: 0;padding: 0;-webkit-text-size-adjust: none;color: #555559;font-family: Arial, sans-serif;font-size: 28px;line-height: 34px;font-weight: bold; text-align: center;">
+                                            <div class="mktEditable" id="main_title">
+                                                Sending Mail from Events Application
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="sub-title" style="border-collapse: collapse;border: 0;margin: 0;padding: 0;padding-top:5px;-webkit-text-size-adjust: none;color: #555559;font-family: Arial, sans-serif;font-size: 18px;line-height: 29px;font-weight: bold;text-align: center;">
+                                        <div class="mktEditable" id="intro_title">
+                                            Activation Code:
+                                        </div></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="top-padding" style="border-collapse: collapse;border: 0;margin: 0;padding: 5px;-webkit-text-size-adjust: none;color: #555559;font-family: Arial, sans-serif;font-size: 16px;line-height: 26px;"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="grey-block" style="border-collapse: collapse;border: 0;margin: 0;-webkit-text-size-adjust: none;color: #555559;font-family: Arial, sans-serif;font-size: 16px;line-height: 26px;background-color: #fff; text-align:center;">
+                                        <div class="mktEditable" id="cta">
+                                        <img class="top-image" src="https://the-events-app.com/img/logo.png" width="560"/><br><br>
+                                            <strong>Date:</strong> {{ date('Y-m-d') }}<br>
+                                            <strong>Time</strong>: {{ date('H:i:s') }}<br><br>
+                                         </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="top-padding" style="border-collapse: collapse;border: 0;margin: 0;padding: 15px 0;-webkit-text-size-adjust: none;color: #555559;font-family: Arial, sans-serif;font-size: 16px;line-height: 21px;">
+                                            <hr size="1" color="#eeeff0">
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text" style="border-collapse: collapse;border: 0;margin: 0;padding: 0;-webkit-text-size-adjust: none;color: #555559;font-family: Arial, sans-serif;font-size: 16px;line-height: 26px;">
+                                        <div class="mktEditable" id="main_text">
+                                            Hello {{$name}},<br><br>
+    
+                                         here is your code : {{$code}}
+                                        </div>
+                                        </td>
+                                    </tr>
+                                     
+    
+                                </table>
+                            </td>
+                        </tr>
+                                                             
+                     
+                        <tr bgcolor="#fff" style="border-top: 4px solid #00a5b5;">
+                            <td valign="top" class="footer" style="border-collapse: collapse;border: 0;margin: 0;padding: 0;-webkit-text-size-adjust: none;color: #555559;font-family: Arial, sans-serif;font-size: 16px;line-height: 26px;background: #fff;text-align: center;">
+                                <table style="font-weight: normal;border-collapse: collapse;border: 0;margin: 0;padding: 0;font-family: Arial, sans-serif;">
+                                    <tr>
+                                        <td class="inside-footer" align="center" valign="middle" style="border-collapse: collapse;border: 0;margin: 0;padding: 20px;-webkit-text-size-adjust: none;color: #555559;font-family: Arial, sans-serif;font-size: 12px;line-height: 16px;vertical-align: middle;text-align: center;width: 580px;">
+    <div id="address" class="mktEditable">
+                                            <b>The Events App</b>
+    </div>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+      </body>
--- resources/views/verify.blade.php
+++
@@ -1,3 +0,0 @@
-<h1>Hi, {{ $name }} Welcome to Events Application</h1>
-<p>here is your code : {{$code}}</p>
-<p>Sending Mail from Events Application.</p>
